9a8c352cd982721131dd48719de0cc084e11b2c7
youzai.sf <youzai.sf@alibaba-inc.com>
2016-06-02 17:30:50 +0800

    1. support save in image mode 2. support read vot groundtruth

--- demo/demo_img_list.cpp
+++ demo/demo_img_list.cpp
@@ -32,11 +32,41 @@ std::vector<std::string> split(std::string in)
     return res;
 }
 
+Rect_T getRectFromRotatedBB(std::vector<std::string> &eles)
+{
+    float ox1 = atof(eles[1].c_str());
+    float oy1 = atof(eles[2].c_str());
+    float ox2 = atof(eles[3].c_str());
+    float oy2 = atof(eles[4].c_str());
+    float ix1 = atof(eles[5].c_str());
+    float iy1 = atof(eles[6].c_str());
+    float ix2 = atof(eles[7].c_str());
+    float iy2 = atof(eles[8].c_str());
+
+    float cx = (ox1+ox2+ix1+ix2)/4.f;
+    float cy = (oy1+oy2+iy1+iy2)/4.f;
+    
+    float x1 = MIN_T(MIN_T(MIN_T(ox1, ox2), ix1), ix2);
+    float x2 = MAX_T(MAX_T(MAX_T(ox1, ox2), ix1), ix2);
+    float y1 = MIN_T(MIN_T(MIN_T(oy1, oy2), iy1), iy2);
+    float y2 = MAX_T(MAX_T(MAX_T(oy1, oy2), iy1), iy2);
+    
+    float A1 = sqrt((ox1-ox2)*(ox1-ox2)+(oy1-oy2)*(oy1-oy2))*sqrt((ix1-ox2)*(ix1-ox2)+(iy1-oy2)*(iy1-oy2));
+    float A2 = (x2 - x1) * (y2 - y1);
+    float s = sqrt(A1/A2);
+    float w = s * (x2 - x1) + 1;
+    float h = s * (y2 - y1) + 1;
+    Rect_T res = {round(cx-w/2), round(cy-h/2),
+                  round(w),round(h)};
+    return res;
+}
+
 int main(int argc, char* argv[]){
     // Parse the options
     char opts[] = "hsm:";
     char oc;
     int method = 0;
+    bool isSave = false;
     while((oc = getopt_t(argc, argv, opts)) != -1)
     {
         switch(oc)
@@ -46,6 +76,9 @@ int main(int argc, char* argv[]){
         case 'm':
             method = atoi(getarg_t());
             break;
+        case 's':
+            isSave = true;
+            break;
         }
     }
     argv += getpos_t();
@@ -53,7 +86,7 @@ int main(int argc, char* argv[]){
     
 	if (argc<1)
         return usage();
-    
+    cv::VideoWriter outputV;
     Image_T img;
     memset(&img, 0, sizeof(Image_T));
     OTHandle handle = ot_create(640, 480, 10, method);
@@ -77,6 +110,23 @@ int main(int argc, char* argv[]){
         if (frame.empty())
             break;
 
+        // Open saved video
+        if (isSave && (!outputV.isOpened()))
+        {
+            char path[1024] = {0};
+            sprintf(path,"%s_res.avi", argv[0]);
+            outputV.open(path,
+                         CV_FOURCC('M','J','P','G'),
+                         25,
+                         cv::Size(frame.cols, frame.rows),
+                         true);
+            if (!outputV.isOpened())
+            {
+                printf("Write video open failed\n");
+                return -1;
+            }
+        }
+
         //char path[1024]={0};
         //sprintf(path, "%s.BMP", eles[0].c_str());
         //imwrite(path, frame);
@@ -90,12 +140,19 @@ int main(int argc, char* argv[]){
         img.data[0] = (unsigned char *)frame.data;
         ot_setImage(handle, &img);
 
+        
         if (isUpdate==false && eles.size()>1)
         {
-            Rect_T roi = { int(atof(eles[1].c_str())),
-                           int(atof(eles[2].c_str())),
-                           int(atof(eles[3].c_str())),
-                           int(atof(eles[4].c_str()))};
+            Rect_T roi;
+            if (5==eles.size())
+            {
+                roi.x = int(atof(eles[1].c_str()));
+                roi.y = int(atof(eles[2].c_str()));
+                roi.w = int(atof(eles[3].c_str()));
+                roi.h = int(atof(eles[4].c_str()));
+            }
+            else
+                roi = getRectFromRotatedBB(eles);
             ot_addObject(handle, &roi, 0);
             isUpdate = true;
             continue;
@@ -112,9 +169,12 @@ int main(int argc, char* argv[]){
                               cv::Point(roi.x, roi.y),
                               cv::Point(roi.x+roi.w,
                                         roi.y+roi.h),
-                              cv::Scalar(0,255,255),1,8);
+                              cv::Scalar(0,255,255),2,8);
             }
         }
+
+        if (isSave)
+            outputV << frame;
         // Show the FPS
         end = timeStamp();   
         char str[50]={0};
